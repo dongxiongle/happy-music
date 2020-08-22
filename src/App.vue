a20bc0c8aee556210a8fbf3569623dff9a952455
杨章波 <yangzhangbo@91jinrong.com>
2020-08-22 18:16:13 +0800

    app

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,35 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue } from 'vue-property-decorator';
+import { namespace } from 'vuex-class';
+import { loginStatusFn } from '@/api';
+import { Toast } from 'vant';
+
+const userState = namespace('user');
 
 @Component({
   components: {}
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+
+  @userState.Mutation('SETPROFILE') SETPROFILE!: Function
+  @userState.Mutation('SETBINDINGS') SETBINDINGS!: Function
+  @userState.Mutation('SETLOGINSTATUS') SETLOGINSTATUS!: Function
+
+  mounted() {
+    loginStatusFn().then((res: any) => {
+      const { code, profile, bindings, message } = res;
+      if (code === 200) {
+        this.SETPROFILE(profile);
+        this.SETBINDINGS(bindings);
+        this.SETLOGINSTATUS(true);
+      } else {
+        Toast(message);
+      }
+    })
+  }
+}
 </script>
 
 <style>
--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -5,11 +5,6 @@
 </template>
 <script lang='ts'>
 import { Vue, Component } from 'vue-property-decorator';
-import { namespace } from 'vuex-class';
-import { loginStatusFn } from '@/api';
-import { Toast } from 'vant';
-
-const userState = namespace('user');
 
 @Component({
   components: {},
@@ -18,26 +13,8 @@ const userState = namespace('user');
 export default class Setting extends Vue {
   name = 'setting';
 
-  @userState.Mutation('SETPROFILE') SETPROFILE!: Function
-  @userState.Mutation('SETBINDINGS') SETBINDINGS!: Function
-  @userState.Mutation('SETLOGINSTATUS') SETLOGINSTATUS!: Function
-
   toLogin() {
     this.$router.push({ name: 'login' });
-  }
-
-  mounted() {
-    loginStatusFn().then((res: any) => {
-      console.log(res);
-      const { code, profile, bindings, message } = res;
-      if (code === 200) {
-        this.SETPROFILE(profile);
-        this.SETBINDINGS(bindings);
-        this.SETLOGINSTATUS(true);
-      } else {
-        Toast(message);
-      }
-    })
   }
 }
 </script>
